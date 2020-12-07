d0339bcce671a4f1ba54e38021482823048ea4bb
Thao <thaotruong199826@gmail.com>
2020-12-07 15:55:21 +0700

    xong chuong 3

---
+++ Chuong3/Bai2_Chuong3/Bai2_Chuong3/Form1.Designer.cs
@@ -0,0 +1,96 @@
+﻿namespace Bai2_Chuong3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnNhap = new System.Windows.Forms.Button();
+            this.txbKQ = new System.Windows.Forms.RichTextBox();
+            this.btnIn = new System.Windows.Forms.Button();
+            this.btnSapXep = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnNhap
+            // 
+            this.btnNhap.Location = new System.Drawing.Point(548, 119);
+            this.btnNhap.Name = "btnNhap";
+            this.btnNhap.Size = new System.Drawing.Size(84, 26);
+            this.btnNhap.TabIndex = 1;
+            this.btnNhap.Text = "Nhập mảng";
+            this.btnNhap.UseVisualStyleBackColor = true;
+            this.btnNhap.Click += new System.EventHandler(this.btnNhap_Click);
+            // 
+            // txbKQ
+            // 
+            this.txbKQ.Location = new System.Drawing.Point(172, 127);
+            this.txbKQ.Name = "txbKQ";
+            this.txbKQ.Size = new System.Drawing.Size(353, 186);
+            this.txbKQ.TabIndex = 2;
+            this.txbKQ.Text = "";
+            // 
+            // btnIn
+            // 
+            this.btnIn.Location = new System.Drawing.Point(302, 319);
+            this.btnIn.Name = "btnIn";
+            this.btnIn.Size = new System.Drawing.Size(84, 26);
+            this.btnIn.TabIndex = 3;
+            this.btnIn.Text = "In mảng";
+            this.btnIn.UseVisualStyleBackColor = true;
+            this.btnIn.Click += new System.EventHandler(this.btnIn_Click);
+            // 
+            // btnSapXep
+            // 
+            this.btnSapXep.Location = new System.Drawing.Point(548, 151);
+            this.btnSapXep.Name = "btnSapXep";
+            this.btnSapXep.Size = new System.Drawing.Size(84, 79);
+            this.btnSapXep.TabIndex = 4;
+            this.btnSapXep.Text = "Sắp xếp";
+            this.btnSapXep.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnSapXep);
+            this.Controls.Add(this.btnIn);
+            this.Controls.Add(this.txbKQ);
+            this.Controls.Add(this.btnNhap);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button btnNhap;
+        private System.Windows.Forms.RichTextBox txbKQ;
+        private System.Windows.Forms.Button btnIn;
+        private System.Windows.Forms.Button btnSapXep;
+    }
+}
+
---
+++ Chuong3/Bai2_Chuong3/Bai2_Chuong3/Form1.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Bai2_Chuong3
+{
+    public partial class Form1 : Form
+    {
+        // 4 dòng 5 cột
+        const int sodong = 4;
+        const int socot = 5;
+        // khai bao mảng có 4 dòng 5 cột
+        int[,] array = new int[4, 5];
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnNhap_Click(object sender, EventArgs e)
+        {
+            int i, j;
+            for(i=0;i<sodong; i++)
+            {
+                for(j=0; j<socot; j++)
+                {
+                    array[i, j] = i + j;
+                }
+            }
+        }
+
+        private void btnIn_Click(object sender, EventArgs e)
+        {
+            int i, j;
+            for (i = 0; i < sodong; i++)
+            {
+                for(j=0; j<socot; j++)
+                {
+                    this.txbKQ.Text += array[i,j] + " ";
+                }
+                this.txbKQ.Text += "\n\r";
+            }
+            // hien thi phan tu nho nhat
+            int ptnhonhat = PhanTuNhoNhat();
+            // hien thi phan tu lon nhat
+            int ptlonnhat = PhanTuLonNhat();
+            // hien thị tong cac gia tri
+            int tongcacgiatri = TongCacGiaTri();
+            // trung binh cac phan tu
+            double trungbinhcacphantu = TinhTrungBinhCong();
+            this.txbKQ.AppendText("\nIn giá trị nhỏ nhất: " + ptnhonhat + "\n\r"+ 
+                                    "In giá trị lớn nhất: "+ptlonnhat+ "\n\r"+
+                                    "Tổng các giá trị: "+tongcacgiatri + "\n\r"+
+                                    "Trung bình các giá trị: "+String.Format("{0:0.00}",trungbinhcacphantu));
+        }
+
+        // in phần tử nhỏ nhất trong mảng
+
+        public int PhanTuNhoNhat()
+        {
+            int i, j;
+            int ptnhonhat = Convert.ToInt32(array[0,0]);
+            for(i=0; i<sodong; i++)
+            {
+                for(j=0; j<socot; j++)
+                {
+                    if (ptnhonhat >= array[i, j])
+                    {
+                        ptnhonhat = array[i, j];
+                    }
+                }
+            }
+            return ptnhonhat;
+        }
+
+        // in phan tu lon nhat
+        public int PhanTuLonNhat()
+        {
+            int i, j;
+            int ptlonnhat = Convert.ToInt32(array[0,0]);
+            for(i=0; i<sodong; i++)
+            {
+                for(j=0; j < socot; j++)
+                {
+                    if(ptlonnhat <= array[i, j])
+                    {
+                        ptlonnhat = array[i, j];
+                    }
+                }
+            }
+            return ptlonnhat;
+        }
+
+        // tổng số các giá trị
+        public int TongCacGiaTri()
+        {
+            int i, j;
+            int tong = 0;
+            for(i=0; i<sodong; i++)
+            {
+                for(j=0; j<socot; j++)
+                {
+                    tong += array[i, j];
+                }
+            }
+
+            return tong;
+        }
+
+        // tinh trung binh cong
+        public double TinhTrungBinhCong()
+        {
+            int tong = TongCacGiaTri();
+            int cacptu = sodong * socot;
+            double tb = tong / (cacptu * 1.0);
+            return tb;
+        }
+
+    }
+}
